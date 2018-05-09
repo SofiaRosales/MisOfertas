c1d80b18d23ff5c16cfec87d1cf3dcdeb15e999d
Sergio Cerda <sergio.cerda21@outlook.com>
2018-05-09 16:45:14 -0300

    a

--- LoginOfertas3/App_Start/RouteConfig.cs
+++ LoginOfertas3/App_Start/RouteConfig.cs
@@ -19,12 +19,6 @@ namespace LoginOfertas3
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
-
-            routes.MapRoute(
-               name: "Default",
-               url: "{controller}/{action}/{id}",
-               defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-           );
         }
     }
 }
